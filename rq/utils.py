a31f96f0d6ea71ce5de4f21dfbcb82de8e6882ab
CrimsonGlory <CrimsonGlory@users.noreply.github.com>
2025-06-27 17:10:29 -0300

    Update utils.py
    raise from

--- rq/utils.py
+++ rq/utils.py
@@ -137,7 +137,7 @@ def import_attribute(name: str) -> Callable[..., Any]:
     except AttributeError:
         raise ValueError('Invalid attribute name: %s' % attribute_name)
     except Exception as e:
-        raise ValueError(f"Unexpected error while getting attribute '{attribute_owner_name}' from '{name}': {e}")
+        raise ValueError(f"Unexpected error while getting attribute '{attribute_owner_name}' from '{name}': {e}") from e
 
     if not hasattr(attribute_owner, attribute_name):
         raise ValueError('Invalid attribute name: %s' % name)
